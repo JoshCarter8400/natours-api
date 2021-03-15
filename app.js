b20fb0f9fde05d38c32698b50bd236c2b0613d7f
Josh Carter <joshcarter8400@gmail.com>
2021-03-15 14:27:54 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello form the server side', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can now post!!!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App is running on ${port}...`);
+});
